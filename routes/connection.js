849e4cb438b7bf57b1eb4bda5efa98a20d2f58a5
ashfullstack <akashramsonule@gmail.com>
2024-11-25 16:52:49 +0530

    Update connection.js

--- routes/connection.js
+++ routes/connection.js
@@ -1,12 +1,20 @@
 var mysql = require("mysql");
 var util = require("util");
 
-var conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"dica_an_dico"
-})
+// var conn = mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"",
+//     database:"dica_an_dico"
+// })
+
+var connection = mysql.createConnection({
+  host: "mysql-21ffbda8-akashramsonule-0c32.b.aivencloud.com", // e.g., "mysql.render.com"
+  user: "avnadmin",
+  password: "AVNS_rKdpWGqMQ-DVDJwwQv4",
+  database: "defaultdb",
+});
+
 
 var exe = util.promisify(conn.query).bind(conn);
 
